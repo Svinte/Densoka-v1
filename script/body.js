592b678a9635bdaaa0b3e7b71f9d525aa6e13d5f
ff617 <99759858+ff617@users.noreply.github.com>
2022-11-05 15:56:57 +0200

    Add files via upload

--- script/body.js
+++ script/body.js
@@ -3,25 +3,10 @@ const body = document.body;
 const banner = document.createElement("div");
 banner.id="Banner";
 body.appendChild(banner);
-const bannertext = document.createElement("h1");
-bannertext.id="bannerText";
-banner.appendChild(bannertext);
 const style = document.createElement("link");
 style.href="/style/body.css";
 style.rel="stylesheet";
 style.type="text/css";
-document.head.appendChild(style);
-const selection = document.createElement("div");
-selection.id="§";
-selection.style.height = window.innerHeight+"px";
-body.appendChild(selection);
-const opse = document.createElement("button");
-opse.id = "open";
-opse.innerHTML="&#9776;";
-bannertext.appendChild(opse);
-const exit = document.createElement("h1");
-exit.id="exit";
-exit.innerText="X";
 const b1 = document.createElement("h1");
 b1.innerText="Koti";
 b1.className="sb";
@@ -46,32 +31,70 @@ b7.className="sb";
 const b8 = document.createElement("h1");
 b8.innerText="muuta densokasta";
 b8.className="sb";
-selection.appendChild(exit);
-selection.appendChild(b1);
-b1.style.marginTop="17vh";
-selection.appendChild(b2);
-selection.appendChild(b3);
-selection.appendChild(b4);
-selection.appendChild(b5);
-selection.appendChild(b6);
-selection.appendChild(b7);
-selection.appendChild(b8);
-document.getElementById("exit").onclick = close;
-document.getElementById("open").onclick = open;
-document.onkeyup = function (p) {
-if (p.keyCode == 32 || p.keyCode == 13 || p.keyCode == 27 || p.keyCode == 17) {
-        if (document.getElementById("open").style.display == "none") {
-            close();
-        }   else open();
+document.head.appendChild(style);
+if (window.innerWidth<800) {
+    const bannertext = document.createElement("h1");
+    bannertext.id="bannerText";
+    banner.appendChild(bannertext);
+    const selection = document.createElement("div");
+    selection.id="§";
+    selection.style.height = window.innerHeight+"px";
+    body.appendChild(selection);
+    const opse = document.createElement("button");
+    opse.id = "open";
+    opse.innerHTML="&#9776;";
+    bannertext.appendChild(opse);
+    const exit = document.createElement("h1");
+    exit.id="exit";
+    exit.innerText="X";
+    selection.appendChild(exit);
+    selection.appendChild(b1);
+    b1.style.marginTop="17vh";
+    selection.appendChild(b2);
+    selection.appendChild(b3);
+    selection.appendChild(b4);
+    selection.appendChild(b5);
+    selection.appendChild(b6);
+    selection.appendChild(b7);
+    selection.appendChild(b8);
+    document.getElementById("exit").onclick = close;
+    document.getElementById("open").onclick = open;
+    document.onkeyup = function (p) {
+    if (p.keyCode == 32 || p.keyCode == 13 || p.keyCode == 27 || p.keyCode == 17) {
+            if (document.getElementById("open").style.display == "none") {
+                close();
+            }   else open();
+        }
     }
-}
-function close() {
-    selection.style.display="none";
-    opse.style.display="block";
-}
-function open() {
-    selection.style.display="block";
-    opse.style.display="none";
+    function close() {
+        selection.style.display="none";
+        opse.style.display="block";
+    }
+    function open() {
+        selection.style.display="block";
+        opse.style.display="none";
+    }
+}   else {
+    banner.appendChild(b1);
+    banner.appendChild(b2);
+    banner.appendChild(b3);
+    banner.appendChild(b4);
+    banner.appendChild(b5);
+    banner.appendChild(b6);
+    banner.appendChild(b7);
+    banner.appendChild(b8);
+    var index=0;
+    function forEach() {
+        if (document.getElementsByClassName("sb")[index]) {
+            document.getElementsByClassName("sb")[index].style="display: inline-table; margin-top: 4vh;";
+            if (index !== 0) {
+                document.getElementsByClassName("sb")[index].style.borderLeft="2px solid black";
+            }
+            index++;
+            forEach();
+        }
+    }
+    forEach();
 }
 b1.onclick = function () {
     document.location.href="/index.html";
